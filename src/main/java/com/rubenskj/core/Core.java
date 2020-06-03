1cb787feae549e0e1ebeb8e69ea226779cf1b61e
RubensKj <rubenskleinschmidtjr@gmail.com>
2020-06-03 13:12:48 -0300

    Changing pom and removing test

--- src/main/java/com/rubenskj/core/Core.java
+++ src/main/java/com/rubenskj/core/Core.java
@@ -1,7 +1,5 @@
 package com.rubenskj.core;
 
-import com.rubenskj.core.test.EnviaEmailContact;
-import com.rubenskj.core.test.Invoice;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -10,12 +8,6 @@ public class Core {
     private static final Logger LOGGER = LoggerFactory.getLogger(Core.class);
 
     public static void main(String[] args) {
-        LOGGER.info("Testing");
-
-
-        new Invoice().produce();
-
-        new EnviaEmailContact().enviaEmail();
-
+        LOGGER.info("Subscribe lib is initialized");
     }
 }
