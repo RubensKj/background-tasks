a66e5bb896129211e17e7f2bbbe354e0356edd87
RubensKj <rubenskleinschmidtjr@gmail.com>
2020-06-04 14:45:25 -0300

    Testing constructor private

--- src/test/java/com/rubenskj/core/util/ValidationUtilsTest.java
+++ src/test/java/com/rubenskj/core/util/ValidationUtilsTest.java
@@ -2,10 +2,21 @@ package com.rubenskj.core.util;
 
 import org.junit.Test;
 
+import java.lang.reflect.Constructor;
+
 import static com.rubenskj.core.util.ValidationUtils.validateString;
 import static org.junit.Assert.*;
 
 public class ValidationUtilsTest {
+
+    @Test
+    public void constructorTest() throws NoSuchMethodException {
+        Class<ValidationUtils> clazz = ValidationUtils.class;
+
+        Constructor<ValidationUtils> constructor = clazz.getDeclaredConstructor();
+
+        assertFalse(constructor.isAccessible());
+    }
 
     @Test
     public void validateStringIsEmpty() {
