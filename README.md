a3dc085423d22aa312147c1c0949ae1a7fee5162
RubensKj <rubenskleinschmidtjr@gmail.com>
2020-06-04 14:37:41 -0300

    Creating registerAsSubscribeTest

--- README.md
+++ README.md
@@ -8,7 +8,7 @@
 [![Languages](https://img.shields.io/github/languages/count/RubensKj/background-tasks?color=00cc74&style=flat-square)](#)
 [![License](https://img.shields.io/github/license/RubensKj/background-tasks?color=00cc74&style=flat-square)](https://github.com/RubensKj/background-tasks/blob/master/LICENSE)
 [![Stars](https://img.shields.io/github/stars/RubensKj/background-tasks?color=00cc74&style=flat-square)](https://github.com/RubensKj/background-tasks/stargazers)
-[![Coverage Status](http://img.shields.io/coveralls/RubensKj/background-tasks/master.svg?color=00cc74&style=flat-square)](https://coveralls.io/github/RubensKj/background-tasks?branch=master)
+[![Coverage Status](http://img.shields.io/coveralls/RubensKj/background-tasks/master.svg?style=flat-square)](https://coveralls.io/github/RubensKj/background-tasks?branch=master)
 [![Maven Version](https://maven-badges.herokuapp.com/maven-central/com.github.rubenskj/background-tasks/badge.svg?color=00cc74&style=flat-square)](https://search.maven.org/artifact/com.github.rubenskj/background-tasks/2.0.0/jar)
 [![Contributors](https://img.shields.io/github/contributors/RubensKj/background-tasks?color=00cc74&style=flat-square)](https://github.com/RubensKj/background-tasks/graphs/contributors)
 
--- src/test/java/com/rubenskj/core/entity/SubscribeTest.java
+++ src/test/java/com/rubenskj/core/entity/SubscribeTest.java
@@ -1,7 +1,14 @@
 package com.rubenskj.core.entity;
 
+import com.rubenskj.core.handler.Subscriber;
+import com.rubenskj.core.handler.Subscribers;
+import com.rubenskj.core.interfaces.ICallback;
 import org.junit.jupiter.api.Test;
 
+import java.lang.reflect.Field;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+import java.util.Map;
 import java.util.UUID;
 import java.util.concurrent.atomic.AtomicBoolean;
 import java.util.concurrent.atomic.AtomicInteger;
@@ -54,5 +61,47 @@ public class SubscribeTest {
         Thread.sleep(500);
 
         assertEquals(0, retryConscructor.get());
+    }
+
+    @Test
+    void registerAsSubscribeTest() throws NoSuchMethodException, InvocationTargetException, IllegalAccessException, NoSuchFieldException {
+        String id = UUID.randomUUID().toString();
+        String subscribeName = SubscribeTest.class.getName();
+        int retryTimes = 1;
+        ICallback callback = () -> {
+        };
+        int consumers = 1;
+
+        Subscribe subscribe = new Subscribe(subscribeName, callback);
+
+        Class<?> clazz = subscribe.getClass();
+
+        Method registerAsSubscribe = clazz.getDeclaredMethod("registerAsSubscribe", String.class, String.class, int.class, ICallback.class, int.class);
+
+        registerAsSubscribe.setAccessible(true);
+
+        registerAsSubscribe.invoke(subscribe, id, subscribeName, retryTimes, callback, consumers);
+
+        Map<String, Subscriber> subscribersList = this.getSubscribersList();
+
+        Subscriber subscriber = subscribersList.get(id);
+
+        assertEquals(SubscribeTest.class.getName(), subscriber.getSubscriberName());
+        assertEquals(retryTimes, subscriber.getRetry());
+        assertEquals(callback, subscriber.getCallback());
+    }
+
+    Map<String, Subscriber> getSubscribersList() throws NoSuchFieldException, IllegalAccessException {
+        Subscribers subscribers = new Subscribers();
+
+        Class<?> clazz = subscribers.getClass();
+
+        Field field = clazz.getDeclaredField("SUBSCRIBERS");
+
+        field.setAccessible(true);
+
+        Map<String, Subscriber> subscribersList = (Map<String, Subscriber>) field.get(subscribers);
+
+        return subscribersList;
     }
 }
